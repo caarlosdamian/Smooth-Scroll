e223bd205e4b42c6714d12f168a11aee2970d087
caarlosdamian <dapecarlos@gmail.com>
2021-08-17 19:48:41 -0500

    add new components and some styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Main</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
